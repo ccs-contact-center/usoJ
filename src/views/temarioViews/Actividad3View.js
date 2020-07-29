79a6e6efe77fa9c15db2be30fefdb8ac0a1b7144
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-29 12:25:09 -0500

    Update Actividad3View.js

--- src/views/temarioViews/Actividad3View.js
+++ src/views/temarioViews/Actividad3View.js
@@ -47,7 +47,7 @@ class Actividad3View extends Component {
   async onSave(e) {
     try {
       var respuesta = await API.guardaActividad(this.state)
-      alert('Se guardo la actividad, con id: ' + respuesta[0].id)
+      alert('Se guardo actividad: 3, con id: ' + respuesta[0].id)
     } catch (err) {
       console.log('loggea si hay un error')
     }
