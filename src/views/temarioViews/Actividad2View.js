ef59dc376da99de37724f82a271c2876d8fa5658
DanielDO93 <61757109+DanielDO93@users.noreply.github.com>
2020-07-29 12:25:05 -0500

    Update Actividad2View.js

--- src/views/temarioViews/Actividad2View.js
+++ src/views/temarioViews/Actividad2View.js
@@ -50,7 +50,7 @@ class ActividadView extends Component {
   async onSave(e) {
     try {
       var respuesta = await API.guardaActividad(this.state)
-      alert('Se guardo la actividad, con id: ' + respuesta[0].id)
+      alert('Se guardo actividad: 2, con id: ' + respuesta[0].id)
     } catch (err) {
       console.log('loggea si hay un error')
     }
